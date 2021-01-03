539e357ef6b908e9e4f78aabdc875b321a843f13
smy-101 <shimy1011@foxmail.com>
2021-01-03 16:37:11 +0800

    创建Button、Input组件

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 
 let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
-try {importAll(require.context('icons', true, /\.svg$/));} catch (error) {console.log(error);}
+try {
+    importAll(require.context('icons', true, /\.svg$/));
+} catch (error) {
+    console.log(error);
+}
 
 type Props = {
-  name: string
+    name?: string
 }
 
 const Icon = (props: Props) => {
-  return (
-    <svg className="icon">
-      <use xlinkHref={'#' + props.name}/>
-    </svg>
-  );
+    return (
+        <svg className="icon">
+            {props.name && <use xlinkHref={'#' + props.name}/>}
+        </svg>
+    );
 };
 
 export default Icon;
