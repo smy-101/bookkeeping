32f77eecec51efaaadc7bbbae448d65dde80b042
smy-101 <shimy1011@foxmail.com>
2021-01-03 13:49:36 +0800

    抽离useTags函数

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import {Layout} from '../components/Layout';
+import {useTags} from 'useTags';
 
 function Tags() {
+    const {tags,setTags}=useTags();
     return (
         <Layout>
             <h2>标签</h2>
