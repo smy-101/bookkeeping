be43ad71bcf18ec97d53ae8fd48089eab33386d3
smy-101 <shimy1011@foxmail.com>
2021-01-03 19:45:35 +0800

    实现Tag页面标签编辑

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -12,7 +12,24 @@ const defaultTags = [
 const useTags = () => {
     const [tags, setTags] = useState<{ id: number; name: string }[]>(defaultTags);
     const findTag = (id:number)=>tags.filter(tag=>tag.id===id)[0]
-    return {tags: tags, setTags: setTags,findTag};
+    const findTagIndex = (id:number)=>{
+        let result = -1;
+        for(let i=0;i<tags.length;i++){
+            if(tags[i].id===id){
+                result=i;
+                break;
+            }
+        }
+        return result;
+    };
+    const updateTag = (id:number,obj:{name:string})=>{
+        const index = findTagIndex(id);
+        //由于不能直接修改数据，需要拷贝一份再修改
+        const tagsClone = JSON.parse(JSON.stringify(tags));
+        tagsClone.splice(index,1,{id:id,name:obj.name});
+        setTags(tagsClone);
+    }
+    return {tags: tags, setTags: setTags,findTag,findTagIndex,updateTag};
 };
 
 export {useTags};
