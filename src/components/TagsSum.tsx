74aeebbf10fd1532b4ac4bde7c40788d1002aa29
smy-101 <shimy1011@foxmail.com>
2021-03-02 22:44:12 +0800

    修改标题

--- src/components/TagsSum.tsx
+++ src/components/TagsSum.tsx
@@ -13,14 +13,11 @@ type data = {
 
 const TagsSum: React.FC<Props> = (props) => {
     const {tags,getName} = useTags();
-    console.log(tags);
 
     let tagIds: number[] = [];
     tags.forEach((t) => {
         tagIds.push(t.id);
     });
-    // let y=0;
-    // const x = (props.monthExpend.filter(r=>r.tagIds[0]===2)).forEach((r)=>{y=NP.plus(r.amount,y)});
 
     let data:data[]=[];
     for (let i=0;i<tagIds.length;i++){
